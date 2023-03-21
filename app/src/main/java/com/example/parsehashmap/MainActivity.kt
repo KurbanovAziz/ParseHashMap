2b9c7df2f32aa5a31c8992948c58e17d9068169e
KurbanovAziz <ripplemobile29@gmail.com>
2023-03-21 14:45:03 +0600

    MainActivity = add fun parseDownloadUrl()

--- app/src/main/java/com/example/parsehashmap/MainActivity.kt
+++ app/src/main/java/com/example/parsehashmap/MainActivity.kt
@@ -1,33 +1,43 @@
-import android.net.Uri
+package com.example.parsehashmap
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import com.example.parsehashmap.databinding.ActivityMainBinding
+import java.net.URLDecoder
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val appLink = referrer?.toString()
-        val linkParams = appLink?.let { parseLinkParams(it) }
-        linkParams?.let { params ->
-            params.forEach { (key, value) ->
-                println("$key: $value")
-            }
-        }
+        parseDownloadUrl()
     }
 
-    private fun parseLinkParams(link: String): HashMap<String, String> {
-        val params = hashMapOf<String, String>()
-        val uri = Uri.parse(link)
-        uri.queryParameterNames.forEach { paramName ->
-            uri.getQueryParameter(paramName)?.let { paramValue ->
-                params[paramName] = paramValue
+    private fun parseDownloadUrl() {
+        val downloadUrl = intent?.getStringExtra(Intent.EXTRA_REFERRER)
+
+        val params = downloadUrl?.let { parseUrlParams(it) }
+
+        Log.d("ololo", "Источник: ${params?.get("utm_source")}")
+        Log.d("ololo", "Кампания: ${params?.get("utm_campaign")}")
+    }
+
+    private fun parseUrlParams(url: String): HashMap<String, String> {
+        val params = HashMap<String, String>()
+        val queryStart = url.indexOf('?')
+        if (queryStart >= 0) {
+            val query = url.substring(queryStart + 1)
+            for (param in query.split('&')) {
+                val pair = param.split('=')
+                val key = URLDecoder.decode(pair[0], "UTF-8")
+                val value = if (pair.size > 1) URLDecoder.decode(pair[1], "UTF-8") else ""
+                params[key] = value
             }
         }
         return params
